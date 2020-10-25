8b7b1aceea9be09e2da1945f99a9d2e83f1dc135
Freddie Nelson <freddie0208@hotmail.com>
2020-10-25 19:34:50 +0000

    cards and pile done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="pile">
+      <Card  
+        v-for="(card) in pile" 
+        :key="card.id" 
+        :color="card.color" 
+        :number="card.number" 
+        :offsetX="card.offsetX"
+        :offsetY="card.offsetY"
+        :pileRotate="card.rotate"
+        :pile="true" 
+      />
+    </div>
+    <div class="cards">
+      <Card 
+        v-for="(card, i) in cards" 
+        :key="card.id" 
+        :color="card.color" 
+        :number="card.number" 
+        :style="{ zIndex: i }" 
+        :length="cards.length" 
+        :index="i"
+        @clicked="cardClicked"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+import uniqid from "uniqid";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      pile: [],
+      cards: [
+        { color: "red", number: 0, id: uniqid.time() },
+        { color: "red", number: 9, id: uniqid.time() },
+        { color: "red", number: 11, id: uniqid.time() },
+        { color: "green", number: 3, id: uniqid.time() },
+        { color: "green", number: 7, id: uniqid.time() },
+        { color: "green", number: 12, id: uniqid.time() },
+        { color: "yellow", number: 3, id: uniqid.time() },
+        { color: "yellow", number: 11, id: uniqid.time() },
+        { color: "yellow", number: 13, id: uniqid.time() },
+        { color: "blue", number: 2, id: uniqid.time() },
+        { color: "blue", number: 5, id: uniqid.time() },
+      ]
+    }
+  },
+  methods: {
+    cardClicked(e) {
+      this.cards.splice(e.index, 1);
+      this.cards = [...this.cards];
+
+      e.card.id = uniqid.time();
+      this.pile.push(e.card);
+    } 
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+  width: 100%;
+  height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  background-color: #780E09;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.cards {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 50px;
+  margin-top: auto;
+}
+
+.pile {
+  width: 300px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: rotateX(55deg);
+  position: absolute;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,189 @@
+<template>
+  <div
+    class="card"
+    :class="{ pile, animate }"
+    :style="{
+      marginTop,
+      transform: animate ? animate : pile
+        ? `rotate(${pileRotate}) scale(.95) translateX(${offsetX}px) translateY(${offsetY}px) !important`
+        : `rotate(${rotate})`,
+      backgroundPositionY: -bgY + 'px',
+      backgroundPositionX: -bgX + 'px',
+    }"
+    @click="clicked"
+    @transitionend="emit ? removeCard() : null"
+  />
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data() {
+    return {
+      height: 197,
+      width: 127,
+      marginTop: 0,
+      bgY: 0,
+      bgX: 0,
+      animate: null,
+      rotate: "",
+      emit: false,
+      offsets: {}
+    };
+  },
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+    },
+    length: {
+      type: Number,
+      default: 0,
+    },
+    color: {
+      type: String,
+      default: "red",
+    },
+    number: {
+      type: Number,
+      default: null,
+    },
+    pile: {
+      type: Boolean,
+      default: false,
+    },
+    offsetX: {
+      type: Number,
+      default: 0
+    },
+    offsetY: {
+      type: Number,
+      default: 0
+    },
+    pileRotate: {
+      type: String,
+      default: ""
+    },
+  },
+  methods: {
+    removeCard() {
+      this.emit = false;
+
+      this.$emit("clicked", {
+        index: this.index,
+        card: {
+          color: this.color,
+          number: this.number,
+          ...this.offsets
+        }
+      });
+    },
+    clicked(e) {
+      if (this.pile) return;
+
+      const { x, y } = e.target.getBoundingClientRect()
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+
+      const rotate = Math.floor(Math.random() * 140) - 70 + "deg";
+
+      const offsetX = Math.floor(Math.random() * 20) - 10;
+      const offsetY = Math.floor(Math.random() * 20) - 10;
+
+      const transform = `translate(${((x - centerX / 1.1) + offsetX) * -1}px, ${((y - centerY + this.height / 1.3) + offsetY) * -1}px) rotateX(55deg) rotate(${rotate}) scale(.95) !important`;
+      this.animate = transform;
+
+      this.offsets = { 
+        offsetX,
+        offsetY,
+        rotate
+      }
+
+      this.emit = true;
+    },
+    calculateColor() {
+      const gap = 1.85;
+
+      switch (this.color) {
+        case "red":
+          this.bgY = 0;
+          break;
+        case "green":
+          this.bgY = this.height + gap;
+          break;
+        case "yellow":
+          this.bgY = (this.height + gap) * 2;
+          break;
+        case "blue":
+          this.bgY = (this.height + gap) * 3;
+          break;
+        case "changeColor":
+          this.bgY = (this.height + gap) * 4;
+          break;
+        case "plus4":
+          this.bgY = (this.height + gap) * 5;
+          break;
+        default:
+          break;
+      }
+    },
+    calculateNumber() {
+      const number = this.number === 0 ? 10 : this.number;
+      this.bgX = (this.width + 1.85) * (number - 1);
+    },
+  },
+  mounted() {
+    const margin = 1.4 * (this.index - this.length / 2);
+    const rotate = 0.5 * margin;
+
+    if (margin < 0) {
+      this.marginTop = margin * -1 + "px";
+      this.rotate = rotate + "deg";
+    } else {
+      this.marginTop = margin + "px";
+      this.rotate = rotate + "deg";
+    }
+
+    this.calculateColor();
+    this.calculateNumber();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 127px;
+  height: 197px;
+  background: url("../assets/spritesheet.jpg") no-repeat;
+  background-size: 1317%;
+  border-radius: 14px;
+  box-shadow: 0px 0px 15px 0px #00000073;
+  transition: transform 0.3s ease, margin-left 0.2s ease, box-shadow 0.2s ease;
+  transition-delay: 0.2s;
+  cursor: pointer;
+
+  &:not(:first-of-type) {
+    margin-left: -60px;
+  }
+
+  &:hover {
+    transform: translateX(-45px) translateY(-45px) rotate(-5deg) !important;
+    margin-left: 1px;
+    box-shadow: 0px 0px 10px 8px #ffe23f, inset 0px 0px 3px 3px #ffe448;
+  }
+
+  &.pile {
+    position: absolute;
+    margin-left: 0px;
+
+    &:hover {
+      margin-left: 0;
+      box-shadow: 0px 0px 15px 0px #00000073;
+    }
+  }
+
+  &.animate {
+    margin-left: -127px !important;
+  }
+}
+</style>
