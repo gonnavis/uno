d3524e7e4c79a6a5813e5f3530169fce4ad99acd
Freddie Nelson <freddie0208@hotmail.com>
2020-10-26 17:07:47 +0000

    added router views

---
+++ client/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import Game from "../views/Game.vue";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/game',
+    name: 'Game',
+    component: Game
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ client/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Scuffed Uno</h1>
+    <h2>By Freddie</h2>
+    
+    <form>
+      <label for="roomCode">Room Code</label>
+      <input type="text" name="roomCode">
+      <button>Join</button>
+    </form>
+
+    <button class="create-btn">Create Room</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
