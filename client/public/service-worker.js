4d283fc4058451678532c1edd7e40ee60baf200b
Freddie Nelson <freddie0208@hotmail.com>
2021-03-14 20:12:05 +0000

    fix

---
+++ client/public/service-worker.js
@@ -0,0 +1,10 @@
+workbox.core.setCacheNameDetails({ prefix: "scuffed-uno" });
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- client/vue.config.js
+++ client/vue.config.js
@@ -5,4 +5,19 @@ module.exports = {
       return args;
     });
   },
+  pwa: {
+    name: "Scuffed Uno",
+    themeColor: "#570001",
+    msTileColor: "#570001",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "default",
+    manifestPath: "public/site.webmanifest",
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "public/service-worker.js",
+      // ...other Workbox options...
+      exclude: [/\.map$/, /_redirects/],
+    },
+  },
 };
